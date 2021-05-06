807765e171af55b6494fc1bb7fb0aafeb0c430d3
HrvojeSimanovic <hrvoje.simanovic@gmail.com>
2021-05-06 11:40:51 +0200

    dodan button AddExpense i Cancel

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,22 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setIsEditing(false);
+  };
+
+  const addNewExpenseHandler = () => {
+    setIsEditing(true);
+  };
+
+  const cancelNewExpenseHandler = () => {
+    setIsEditing(false);
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isEditing && (
+        <button onClick={addNewExpenseHandler}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={cancelNewExpenseHandler}
+        />
+      )}
     </div>
   );
 };
 
 export default NewExpense;
+
+// console.log(isEditing);
